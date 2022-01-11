9b7a8649bb6cc453dbe1c68596d689a2b60a28d2
Sumukh Moudgalya <you@example.com>
2022-01-12 01:11:06 +0530

    "Service Review and Shop Review Fragment has been added"

--- app/src/main/java/com/defines/bloomerbarber/LoginActivity.kt
+++ app/src/main/java/com/defines/bloomerbarber/LoginActivity.kt
@@ -55,7 +55,7 @@ class LoginActivity : AppCompatActivity() {
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestIdToken("1027298884319-ms19fv47qd0rcq2dd9treomku12f5rnn.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
